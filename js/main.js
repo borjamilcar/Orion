5d0e9e282af255f47d94424dcbfe989b5a2d46db
borjamilcar <borjamilcar01@gmail.com>
2018-08-14 02:59:22 +0100

    Finished Today -_-

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$("#slider").owlCarousel({
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+});
